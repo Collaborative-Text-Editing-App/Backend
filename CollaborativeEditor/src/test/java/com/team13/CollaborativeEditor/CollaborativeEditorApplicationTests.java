b13426dbb0e49e59f297f5d291170f82514e17ea
Mario Fouad <mariofouad03@gmail.com>
2025-03-22 15:24:24 +0200

    initial files

---
+++ CollaborativeEditor/src/test/java/com/team13/CollaborativeEditor/CollaborativeEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team13.CollaborativeEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
