b317d19283e46446f6c53c780e74e4b915baa025
Mario Fouad <mariofouad03@gmail.com>
2025-03-29 20:22:47 +0200

    init

--- CollaborativeEditor/src/main/java/com/team13/CollaborativeEditor/CollaborativeEditorApplication.java
+++ CollaborativeEditor/src/main/java/com/team13/CollaborativeEditor/CollaborativeEditorApplication.java
@@ -2,6 +2,8 @@ package com.team13.CollaborativeEditor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CollaborativeEditorApplication {
